07c130107cb5efbfac3d64aa7787e4e0ac2e45ba
David Kharazyan <50180326+DaveMSU@users.noreply.github.com>
2020-07-12 20:28:46 +0500

    Add files via upload

--- Codeforces Round #655 (Div. 2)/A/solution.cpp
+++ Codeforces Round #655 (Div. 2)/A/solution.cpp
@@ -1,1 +1,17 @@
-asd
+#include <iostream>
+
+int main(){
+
+	size_t t, n;
+	std::cin >> t;
+	
+	while( t-- ){
+	
+		std::cin >> n;		
+		while( n-- )
+			std::cout << 1 << " ";
+		std::cout << std::endl;
+	}	
+
+	return 0;
+}
